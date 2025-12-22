3f3424c0f8a51c18cc053fea2097184f43ca90c6
Jignesh <solankijignesh4611@gmail.com>
2025-12-22 17:50:29 +0530

    till context use Ustate further going for use Reducer

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,23 +1,72 @@
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom'
+import { useEffect, useState } from 'react';
 import styles from './Map.module.css'
-
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvent } from 'react-leaflet';
+import { useCities } from '../contexts/CitiesContext';
+import Spinner from './Spinner';
+import { useGeolocation } from '../hooks/useGeoLocation';
+import { useUrlPosition } from '../hooks/useUrlPosition';
+import Button from './Button';
 function Map() {
-  const navigate= useNavigate();
-  const [searchParams, setSearchParams]= useSearchParams();
-  const lat=searchParams.get("lat");
-  const lng=searchParams.get("lng");
-  
+
+  const navigate = useNavigate();
+  const { cities, isLoading } = useCities();
+  const [mapPosition, setMapPosition] = useState([23.107487, 72.5968327])
+  const { isLoading: isLoadingPosition, position: geoLocationPosition, getPosition } = useGeolocation();
+ const[lat,lng] = useUrlPosition();
+
+  useEffect(function () {
+    if (lng & lng) setMapPosition([lat, lng]);
+
+  }, [lat, lng])
+
+  useEffect(function() {
+      if(geoLocationPosition)setMapPosition([geoLocationPosition.lat, geoLocationPosition.lng])
+  },[geoLocationPosition])
+
+
+  if (isLoading) return <Spinner />;
+
   return (
 
-    <div className={styles.mapContainer} onClick={()=>navigate("form")}>
-      <h1>MAP</h1>
-      <h1>Position {lat} ,{lng} </h1>
+    <div className={styles.mapContainer}>
+     {!geoLocationPosition && <Button type='position' onClick={getPosition}>
+        {isLoadingPosition ? "Loading ..." : "Use your position"}
+      </Button>}
+      <MapContainer
+        center={mapPosition}
+        // center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+        className={styles.map}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map(city => (<Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+          <Popup>
+            <span>{city.emoji}</span><span>{city.cityName}</span>
+          </Popup>
+        </Marker>))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
+      </MapContainer>
 
-      <button onClick={()=>{
-        setSearchParams({lat : 23, lng : 50})
-      }}>Change position</button>
     </div>
   )
+}
+function ChangeCenter({ position }) {
+  const map = useMap()
+
+  map.setView(position);
+  return null;
+
+}
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvent({
+    click: e => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
+  })
 }
 
 export default Map
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,17 +1,19 @@
-const { createContext,useState,useEffect } = require("react");
+import { createContext, useState, useEffect, useContext } from "react";
+
 const BASE_URL = "http://localhost:8000";
 
 const CitiesContext = createContext();
 
-function CitiesProvider({children})
-{
-   const [cities, setCities] = useState([])
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [currentCity, setCurrentCity] = useState({})
+
   useEffect(function () {
     async function fetchCities() {
       try {
         setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
+        const res = await fetch(`${BASE_URL}/cities/`);
         const data = await res.json();
         setCities(data);
         console.log(data);
@@ -28,11 +30,70 @@ function CitiesProvider({children})
   }
     , [])
 
-    return <CitiesContext.Provider value={{
-      cities,isLoading,
-    }}>
-{children}
-    </CitiesContext.Provider>
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    }
+    catch {
+      alert('There was an error loading data..')
+    }
+    finally {
+      setIsLoading(false)
+    }
+  }
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers:
+        {
+          "Content-Type": "application/json"
+        }
+      });
+      const data = await res.json();
+      console.log(data);
+      setCurrentCity(data);
+      setCities(cities => [...cities, data])
+    }
+    catch {
+      alert('There was an error loading data..')
+    }
+    finally {
+      setIsLoading(false)
+    }
+  }
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE"
+      });
+      const data = await res.json();
+      setCities(cities => cities.filter(city=>city.id!== id ))
+    }
+    catch {
+      alert('There was an error deleteing city .')
+    }
+    finally {
+      setIsLoading(false)
+    }
+  }
+
+  return <CitiesContext.Provider value={{
+    cities, isLoading, currentCity, getCity, createCity,deleteCity
+  }}>
+    {children}
+  </CitiesContext.Provider>
 
 }
-export {CitiesProvider};
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined) throw new Error("Cities was used outside then CitiesProvider")
+  return context;
+}
+export { CitiesProvider, useCities };

2627d58eb4bf35f1925146f141db1bb9df4e4f01
Jignesh <solankijignesh4611@gmail.com>
2025-12-23 08:36:49 +0530

    authentication complete

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import CountriesList from "./components/CountriesList";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoutes from "./pages/ProtectedRoutes";
 function App() {
 
 
@@ -25,7 +26,7 @@ function App() {
               <Route path="/pricing" element={<Pricing />} />
               <Route path="/login" element={<Login />} />
               <Route path="*" element={<PageNotFound />} />
-              <Route path="/app" element={<AppLayout />}>
+              <Route path="/app" element={<ProtectedRoutes><AppLayout /></ProtectedRoutes>}>
                 <Route index element={<Navigate replace to="cities" />} />
                 <Route path="cities" element={<CityList />} />
                 <Route path='cities/:id' element={<City />} />

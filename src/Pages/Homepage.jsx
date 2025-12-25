97e60d8c9910559dd41e52724ccdab517a4e4401
Jignesh <solankijignesh4611@gmail.com>
2025-12-25 14:32:40 +0530

    Project Complete

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav"
 import styles from "./Homepage.module.css";
 
-export default function Homepage() {
+export default function HomePage() {
   return (
     <main className={styles.homepage}>
       <PageNav/>
